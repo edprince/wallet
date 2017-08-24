70b812479f906f7dbf711e6ae9e8ebe2db03c088
Ed Prince <edward_prince@hotmail.com>
2017-08-24 13:41:08 +0100

    firebase working

--- js/app.js
+++ js/app.js
@@ -1,35 +1,23 @@
 var provider = new firebase.auth.GoogleAuthProvider();
 //provider.addScope("https://www.googleapis.com/auth/contacts.readonly");
-
-window.addEventListener("load", function() {
-  firebase
-    .auth()
-    .signInWithPopup(provider)
-    .then(function(result) {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      var token = result.credential.accessToken;
-      // The signed-in user info.
-      var user = result.user;
-      // ...
-    })
-    .catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // The email of the user's account used.
-      var email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      var credential = error.credential;
-      // ...
-    });
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    alert("You are logged in");
+  } else {
+    // No user is signed in.
+    googleSignIn();
+  }
 });
 
 var addAttributeButton = document.getElementById("add-attribute");
+var saveButton = document.getElementById("save-button");
 addAttributeButton.addEventListener("click", addAttribute);
+saveButton.addEventListener("click", write);
 
 function addAttribute() {
-  var keyContainer = document.getElementById("attribute-key");
-  var valueContainer = document.getElementById("attribute-value");
+  var keyContainer = document.getElementById("key-attribute");
+  var valueContainer = document.getElementById("value-attribute");
   var input = document.createElement("input");
   var labelInput = document.createElement("input");
   input.setAttribute("type", "text");
@@ -38,3 +26,42 @@ function addAttribute() {
   keyContainer.appendChild(labelInput);
   valueContainer.appendChild(input);
 }
+
+function googleSignIn() {
+  firebase.auth().signInWithRedirect(provider).catch(function(error) {
+    //Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    alert(errorMessage);
+  });
+}
+
+function getData() {
+  /**
+   * Function retrieves all data from the fields
+   * @return {result} object - data retrieved from form
+   */
+  var result;
+  result = {
+    natwest: {
+      "account number": "18485748",
+      "sort code": "42-44-09",
+      "card number": "9857-4838-2949-0981",
+      name: "Mr Edward G Prince"
+    }
+  };
+  var keys;
+  return result;
+}
+
+function write(key, data) {
+  /**
+   * Function writes data to database
+   * @param {string} key: The key of the data, e.g "card name"
+   * @param {string} data: The data to be stored, e.g "Ed Prince"
+   */
+  var data = getData();
+  var userId = firebase.auth().currentUser.uid;
+  var database = firebase.database();
+  firebase.database().ref("users/" + userId).set(data);
+
